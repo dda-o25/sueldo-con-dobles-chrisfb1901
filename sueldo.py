64ec58535f0e71a3ff32628f67dcd6218c87adef
chrisfb1901 <christian.flores@iteso.mx>
2025-09-18 23:22:48 -0600

    sueldo_chris

--- sueldo.py
+++ sueldo.py
@@ -4,13 +4,25 @@ Inserta el encabezado aquí y escribe tu código abajo
 
 
 # Declaraciones
-CONSTANTE = valor
+horas_normales=48
 
-# Entradas
-entrada = input()
+#Entradas
+horas_trabajadas = int(input("Horas trabajadas: "))
+tarifa_hora = int(input("Tarifa por hora: "))
 
 # Proceso
+if horas_trabajadas > horas_normales:
+    horas_extras = horas_trabajadas - horas_normales
+    sueldo_extras = horas_extras * tarifa_hora * 2
+    sueldo_normal = horas_normales * tarifa_hora
+else:
+    horas_extras = 0
+    sueldo_extras = 0
+    sueldo_normal = horas_trabajadas * tarifa_hora
 
+sueldo_total = sueldo_normal + sueldo_extras
 
 # Salidas
-print(salida)
+print("Horas extras:", horas_extras)
+print("Sueldo por horas extras:", sueldo_extras)
+print("Sueldo total:", sueldo_total)
